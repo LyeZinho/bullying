caa9253978251132679b6c2f94797871349b718d
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2022-03-21 01:14:48 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express()
+const path = require('path');
 const port = process.env.PORT || 3000;
 
 //Rota para o sub dominio esp [Sub dominio principal]
