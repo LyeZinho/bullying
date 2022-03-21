4663d56cb63b4a2b861fa9286ea61f3744105acd
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2022-03-21 01:07:14 +0000

    Update index.js

--- src/page/index.js
+++ src/page/index.js
@@ -4,15 +4,15 @@ const port = process.env.PORT || 3000;
 
 //Rota para o sub dominio esp [Sub dominio principal]
 
-datarouter.get('/home/css', function(req, res){
+app.get('/home/css', function(req, res){
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-datarouter.get('/home/css', function(req, res){
+app.get('/home/css', function(req, res){
     res.sendFile(path.join(__dirname, 'style.css'));
 });
 
-datarouter.get('/imgsrc/:imagesrc', function(req, res){
+app.get('/imgsrc/:imagesrc', function(req, res){
     res.sendFile(path.join(__dirname, '/img/' + req.params.imagesrc));
 });
 
